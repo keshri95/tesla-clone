30bd1e379c65529ddffcd3452652aa22f9fdbf42
Ashish Keshri <keshri.ashish1@gmail.com>
2022-12-12 19:16:37 +0530

    index.js

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { defaults } from "react-reveal/globals";
 
 const initialState = {
     cars: ["Modal S", "Modal 3", "Modal X", "Modal Y", "Solar Roof", "Solar Pannels"]
